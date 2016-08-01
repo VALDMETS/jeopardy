6a6f55e46cf085e80f10ecd315f293615c97109f
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-07-31 22:18:57 -0500

    added in content security header

--- app/scripts/data/boarddata.js
+++ app/scripts/data/boarddata.js
@@ -4,6 +4,9 @@ import store from '../store';
 
 const BoardData = Bb.Model.extend({
     idAttribute: 'id',
+    headers: {
+      'Content-Security-Policy': 'http://jservice.io'
+    },
     defaults: {
         questionList: []
     },
